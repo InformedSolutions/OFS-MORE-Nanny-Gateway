8ee02f980bbcbfb829c1f976d7291edb6ee5f01e
vagrant <thomas.stokes@informed.com>
2018-07-24 13:43:57 +0100

    __init__.py: Remove redundant import statement.

--- application/models/__init__.py
+++ application/models/__init__.py
@@ -2,7 +2,6 @@ from application.models.declaration import *
 from application.models.nanny_application import *
 from application.models.childcare_training import *
 from application.models.childcare_address import *
-from application.models.base import *
 from application.models.first_aid import *
 from application.models.applicant_personal_details import *
 from application.models.applicant_home_address import *
@@ -10,5 +9,3 @@ from application.models.insurance_cover import *
 from application.models.dbs_check import *
 from application.models.application_reference import *
 from application.models.payment import *
-
-name = "nanny_models"
