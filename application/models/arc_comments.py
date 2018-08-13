7ab5d0a5b35307577165bac81de5028d602aef93
vagrant <thomas.stokes@informed.com>
2018-08-13 08:57:36 +0100

    Revert "Remove redundant table_name."
    This reverts commit f646095e572418ef0ceefa388e1ffe20a4d14d4e.

--- application/models/arc_comments.py
+++ application/models/arc_comments.py
@@ -11,6 +11,7 @@ class ArcComments(models.Model):
 
     review_id = models.UUIDField(primary_key=True, default=uuid4, unique=True),
     table_pk = models.UUIDField(blank=True)
+    table_name = models.CharField(max_length=30, blank=True)
     field_name = models.CharField(max_length=40, blank=True)
     comment = models.CharField(max_length=100, blank=True)
     flagged = models.BooleanField()
