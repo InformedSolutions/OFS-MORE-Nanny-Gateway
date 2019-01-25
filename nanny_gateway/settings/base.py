703bd1fb257c03f1dd9d6afd4b73e6c683a7eb14
James Cruddas <james.cruddas@informed.com>
2019-01-25 11:12:52 +0000

    Settings updates

--- nanny_gateway/settings/base.py
+++ nanny_gateway/settings/base.py
@@ -118,11 +118,11 @@ ALLOWED_HOSTS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
-        'USER': os.environ.get('POSTGRES_USER', 'ofsted'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'OfstedB3ta'),
-        'HOST': os.environ.get('POSTGRES_HOST', '130.130.52.132'),
-        'PORT': os.environ.get('POSTGRES_PORT', '5462')
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT')
     }
 }
 
--- nanny_gateway/settings/dev.py
+++ nanny_gateway/settings/dev.py
@@ -5,3 +5,15 @@ DEBUG = True
 TEST_MODE = True
 
 INSTALLED_APPS = BUILTIN_APPS + THIRD_PARTY_APPS + PROJECT_APPS
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_DB', 'nanny_db'),
+        'USER': os.environ.get('POSTGRES_USER', 'ofs'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'ofs'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432')
+    }
+}
