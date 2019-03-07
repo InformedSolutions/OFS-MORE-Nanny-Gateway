75ee8ee4fa50d031adbbc6d3e2fc272450c7c103
James Cruddas <james.cruddas@informed.com>
2019-03-07 20:02:31 +0000

    Remove further legacy application reference assets

--- nanny_gateway/settings/base.py
+++ nanny_gateway/settings/base.py
@@ -15,8 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-APPLICATION_PREFIX = 'EY'
-
 GOVUK_SERVICE_SETTINGS = {
     'name': 'OFS',
     'phase': 'beta',
