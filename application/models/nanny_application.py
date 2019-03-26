6223820c08b79cfa9fdb7ad6001b48f15dd99697
Mark Frimston <Mark.Frimston@informed.com>
2019-03-26 13:29:44 +0000

    add date_last_accessed field to nanny application

--- application/models/nanny_application.py
+++ application/models/nanny_application.py
@@ -64,6 +64,7 @@ class NannyApplication(models.Model):
     date_updated = models.DateTimeField(blank=True, null=True)
     date_accepted = models.DateTimeField(blank=True, null=True)
     date_submitted = models.DateTimeField(blank=True, null=True)
+    date_last_accessed = models.DateTimeField(blank=True, null=True)
     application_reference = models.CharField(blank=True, null=True, max_length=7,
                                              validators=[RegexValidator(r'([0-9]{7})')])
     ofsted_visit_email_sent = models.DateTimeField(blank=True, null=True)
